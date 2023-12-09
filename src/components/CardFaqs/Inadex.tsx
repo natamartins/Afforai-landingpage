a9ae45a14ee1c10fe72d1d5c63d3fac5a620c80b
natamartins <nata.codedev@gmail.com>
2023-12-09 12:21:21 -0300

    commit section-eleven section-faq

---
+++ src/components/CardFaqs/Inadex.tsx
@@ -0,0 +1,37 @@
+"use client"
+import { ChevronDown, ChevronUp } from 'lucide-react'
+import React, { useState } from 'react'
+
+interface typeCardFaqs {
+    question: string,
+    answer: string
+}
+
+const Inadex = ({ question, answer }: typeCardFaqs) => {
+    const [openFAQ, setOpenFAQ] = useState(false)
+    const handleOpenFaq = () => {
+        setOpenFAQ(!openFAQ)
+    }
+
+    return (
+        <div onClick={handleOpenFaq} className='card_faqs-container'>
+            <div className='card_faqs-box'>
+                <h4>{question}</h4>
+                {
+                    openFAQ ? (
+                        <ChevronUp />
+                    ) : (
+                        <ChevronDown />
+                    )
+                }
+            </div>
+            {
+                openFAQ && (
+                    <p>{answer}</p>
+                )
+            }
+        </div>
+    )
+}
+
+export default Inadex
